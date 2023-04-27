c99b527e1dceab4cb222681f9ff4d173014cf07d
Nina Turner <cfratto87@gmail.com>
2023-04-27 14:08:45 -0500

    Add edit view, adjusted new to use form_with

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence:true, uniqueness: true
-    validates :email, presence:true, 
-    format: {with: /^(.+)@(.+)$/, message: "Email invalid"  }, uniqueness: true
+    validates :email, presence:true, uniqueness: true
     validates :password, presence:true, length: { minimum: 8, too_short: 
         "Password must be a minimum of %{count} characters" }
 end
